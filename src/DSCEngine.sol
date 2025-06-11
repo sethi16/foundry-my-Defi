7fa3ac79af62aa5e8b237579fa982a58732a5b74
Sarthak Sethi <sethisarthak16@gmail.com>
2025-06-11 21:42:19 +0530

    d

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -22,7 +22,7 @@
 
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
