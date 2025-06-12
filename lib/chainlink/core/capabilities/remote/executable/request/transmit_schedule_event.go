e1380892cf65ee7c645ef6b5691659d730291d8d
Sarthak Sethi <sethisarthak16@gmail.com>
2025-06-12 19:01:29 +0530

    last changes

--- lib/chainlink/core/capabilities/remote/executable/request/transmit_schedule_event.go
+++
@@ -1,8 +0,0 @@
-package request
-
-// modify with care - any changes will break downstream kafka consumers.
-const (
-	TransmissionEventSchema   = "github.com/smartcontractkit/chainlink-protos/workflows/workflows/v1/transmit_schedule_event.proto"
-	TransmissionEventProtoPkg = "workflows.v1"
-	TransmissionEventEntity   = "TransmitScheduleEvent"
-)
--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -24,11 +24,11 @@
 
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { DecentralizedStableCoin } from "./DecentralizedStableCoin.sol";
-import "chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-imprt {Oraclelib} from "./src/libraries/Oracle.lib"; 
+import "@chainlink/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {OracleLib} from "./src/libraries/OracleLib.sol"; 
 
 
 
